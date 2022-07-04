1c83d7d2c1722502131320bd54bfead9fa165bc7
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-07-04 15:21:37 -0400

    use dev build script

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -x
 echo "Got architecture $1..."
 git clone --branch v22.1.2 https://github.com/cockroachdb/cockroach
 cd cockroach
-bazel build pkg/cmd/cockroach --config=with_ui --cross=$1
+./dev doctor
+./dev build --cross=$1
 ls -lrt artifacts/
