c443331958e2ecbe020ba58cfce42ae313aa854c
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-07-04 19:27:19 -0400

    incldue a few docker images for debugging

--- manifest.sh
+++ manifest.sh
@@ -6,13 +6,14 @@ export SHA="sha-${SHA::8}"
 # pull the containers
 docker pull ghcr.io/jrcichra/cockroach-linux:${SHA}
 docker pull ghcr.io/jrcichra/cockroach-linuxarm:${SHA}
-
+docker images
 #https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
 
 # update the current sha on the base image
 docker manifest create ghcr.io/jrcichra/cockroach:${SHA} \
     --amend ghcr.io/jrcichra/cockroach-linux:${SHA} \
     --amend ghcr.io/jrcichra/cockroach-linuxarm:${SHA}
+docker images
 docker manifest push ghcr.io/jrcichra/cockroach:${SHA}
 
 # if main branch, update the latest tag on the base image
@@ -20,5 +21,6 @@ if [ "$BRANCH_NAME" == "main" ] || [ "$BRANCH_NAME" == "master" ]; then
     docker manifest create ghcr.io/jrcichra/cockroach:latest \
         --amend ghcr.io/jrcichra/cockroach-linux:${SHA} \
         --amend ghcr.io/jrcichra/cockroach-linuxarm:${SHA}
+    docker images
     docker manifest push ghcr.io/jrcichra/cockroach:latest
 fi
