9b9068bb446b41826ad420888aa15d1586d1f7b2
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-07-04 16:23:43 -0400

    why is bazel like this

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -x
 echo "Got architecture $1..."
 git clone --branch v22.1.2 https://github.com/cockroachdb/cockroach
 cd cockroach
+echo "build --config=crosslinux
+build --config nolintonbuild
+" >> .bazelrc.user
+echo "build --remote_cache=http://127.0.0.1:9867" >> ~/.bazelrc
 ./dev doctor
 ./dev build --cross=$1
 ls -lrt artifacts/
